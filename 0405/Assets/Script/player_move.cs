014a9eb88a98e0ca195f30a01604e91fcc463499
Jaeeun-Lee <wodmsdl0527@naver.com>
2017-04-05 21:11:03 +0900

    0405

---
+++ 0405/Assets/Script/player_move.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_move : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 플레이어를 방향키로 이동시킨다.
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        //Enemy와 Player 간의 충돌 처리
+       
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            Application.LoadLevel("GameOver");
+        }
+    }
+}
