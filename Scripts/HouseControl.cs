4a5f26b770ae2adf8e83b287cc8147281c4d1b2e
Neriel <nerielnexus@gmail.com>
2017-04-25 18:12:55 +0900

    원본 스크립트 추가

---
+++ Scripts/HouseControl.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseControl : MonoBehaviour {
+
+	#region 변수와 프로퍼티
+	private GameObject User;
+
+	// 기준거리 설정
+	public float dist = 2.0f;
+
+	private bool IsCreated = false;
+	public bool CheckIsCreated
+	{
+		get
+		{
+			return IsCreated;
+		}
+
+		set
+		{
+			IsCreated = value;
+		}
+	}
+
+	private GameObject gauge = null;
+	public GameObject CheckGauge
+	{
+		get
+		{
+			return gauge;
+		}
+
+		set
+		{
+			gauge = value;
+		}
+	}
+
+	private GameObject Result = null; 
+	#endregion
+
+
+
+	// Use this for initialization
+	void Start () {
+		if((User = GameObject.FindWithTag("MyPlayer") as GameObject) == null)
+		{
+			Debug.Log("Cannot find Player!");
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// space 를 누름
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			// 기준거리 안에 들어왔고 게이지 생성이 안된 상태라면
+			if (CalcDistance() && !IsCreated)
+			{
+				IsCreated = true;
+				gauge = Instantiate(Resources.Load("ActionGauge")) as GameObject;
+				gauge.transform.position = this.transform.position + new Vector3(0, 0.5f, 0);
+				gauge.transform.rotation = Camera.main.transform.rotation;
+			} 
+			// 게이지 생성이 된 상태라면
+			else if(IsCreated)
+			{
+				// 게이지 액션을 실행하고 결과를 저장
+				bool isSuccess = GameObject.Find("ActionBar").GetComponent<NumberChange>().DoGaugeAction(); ;
+
+				if(isSuccess)
+				{
+					Debug.Log("Success");
+					Destroy(gauge.gameObject);
+
+					Result = Instantiate(Resources.Load("NotifySuccess")) as GameObject;
+					Result.transform.position = this.transform.position + new Vector3(0, 1.0f, 0);
+					Result.transform.rotation = Camera.main.transform.rotation;
+					Destroy(Result.gameObject, 2.0f);
+					
+					IsCreated = false;
+				}
+				else
+				{
+					Debug.Log("Fail");
+					Destroy(gauge.gameObject);
+
+					Result = Instantiate(Resources.Load("NotifyFailure")) as GameObject;
+					Result.transform.position = this.transform.position + new Vector3(0, 1.0f, 0);
+					Result.transform.rotation = Camera.main.transform.rotation;
+					Destroy(Result.gameObject, 2.0f);
+
+					IsCreated = false;
+				}
+			}
+		}
+		
+		// 게이지 생성 후 기준거리보다 멀어지면
+		if(!CalcDistance() && IsCreated)
+		{
+			if (gauge != null)
+				Destroy(gauge.gameObject);
+
+			IsCreated = false;
+		}
+
+		
+	}
+
+	// 거리 계산
+	bool CalcDistance()
+	{
+		if (dist >= Vector3.Distance(User.transform.position, transform.position))
+			return true;
+
+		return false;
+	}
+
+
+}
---
+++ Scripts/NumberChange.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberChange : MonoBehaviour
+{
+	public Image AreaBG;
+	public Image AreaTarget;
+	public Canvas AreaParent;
+
+	#region 변수와 프로퍼티
+	// 게이지 최대값
+	private float count;
+	public float _Count
+	{
+		get
+		{
+			return count;
+		}
+	}
+
+	// 게이지 현재 값
+	private float number;
+	public float _Number
+	{
+		get
+		{
+			return number;
+		}
+	}
+
+	// 게이지 판정영역 설정
+	private int ActionSize;
+	public int _ActionSize
+	{
+		get
+		{
+			return ActionSize;
+		}
+	}
+
+	// 게이지 최고점 확인용
+	private bool isHigh;
+
+	// 게이지 증가량
+	public float rate;
+	#endregion
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(RoundTrip());
+
+		rate = 0.034f;
+		count = 3.4f;
+		number = 0.0f;
+		isHigh = false;
+		ActionSize = 1;
+
+		AreaTarget.rectTransform.sizeDelta = new Vector2(60, 100);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Alpha1))
+			ActionSize = 1;
+
+		if (Input.GetKey(KeyCode.Alpha2))
+			ActionSize = 2;
+
+		if (Input.GetKey(KeyCode.Alpha3))
+			ActionSize = 3;
+
+		switch (ActionSize)
+		{
+			case 1:
+				AreaTarget.rectTransform.sizeDelta = new Vector2(60, 100);
+				break;
+
+			case 2:
+				AreaTarget.rectTransform.sizeDelta = new Vector2(40, 100);
+				break;
+
+			case 3:
+				AreaTarget.rectTransform.sizeDelta = new Vector2(20, 100);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	public IEnumerator RoundTrip()
+	{
+		while (true)
+		{
+			if (number <= 0)
+				isHigh = false;
+			else if (number >= count)
+				isHigh = true;
+
+			if (isHigh)
+			{
+				number -= rate;
+				transform.position -= new Vector3(rate, 0, 0);
+				yield return new WaitForSeconds(0.0f);
+			}
+			else
+			{
+				number += rate;
+				transform.position += new Vector3(rate, 0, 0);
+				yield return new WaitForSeconds(0.0f);
+			}
+
+			yield return null;
+		}
+	}
+
+	public bool DoGaugeAction()
+	{
+		float min = count * 0.1f * (1 + ActionSize);
+		float max = count * (1.0f - 0.1f * (1 + ActionSize));
+
+		if(number >= min && number <= max)
+			return true;
+		else
+			return false;
+	}
+}
